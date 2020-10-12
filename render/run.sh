e8699c9453f1bd6b5fac495d81adec4aa0e67e20
John Jones <johnjones4@gmail.com>
2020-10-12 09:09:20 -0400

    Platform buildout

--- render/run.sh
+++ render/run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-bin/render > rendered.html
-wkhtmltoimage --format bmp --width 825 --height 1200 rendered.html rendered.bmp
-convert -rotate "90" rendered.bmp rendered.bmp
-IT8951/IT8951 0 0 rendered.bmp
-rm rendered.*
-sleep 300
+while true
+do
+  bin/render > rendered.html
+  wkhtmltoimage --format bmp --width 825 --height 1200 rendered.html rendered.bmp
+  convert -rotate "90" rendered.bmp rendered.bmp
+  IT8951/IT8951 0 0 rendered.bmp
+  rm rendered.*
+  sleep 300
+done
