430c9d529945f596257a1f8278e0aafaf730bfc4
John Jones <johnjones4@gmail.com>
2020-10-12 22:32:27 +0100

    installation changes

--- render/run.sh
+++ render/run.sh
@@ -4,8 +4,8 @@ while true
 do
   bin/render > rendered.html
   wkhtmltoimage --format bmp --width 825 --height 1200 rendered.html rendered.bmp
-  convert -rotate "90" rendered.bmp rendered.bmp
-  IT8951/IT8951 0 0 rendered.bmp
+  convert -rotate "-90" rendered.bmp rendered.bmp
+  sudo IT8951/IT8951 0 0 rendered.bmp
   rm rendered.*
   sleep 300
 done
