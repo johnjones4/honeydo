d81085b0e7a83cdc2c4dadf76934e8fdbca27d3c
John Jones <johnjones@battlestation.house.johnjonesfour.com>
2021-11-08 17:05:47 -0500

    not sure

---
+++ render/list.go
@@ -0,0 +1,12 @@
+package render
+
+import "image"
+
+type ListItem struct {
+	Title    string
+	Subtitle string
+}
+
+func renderList(width, height int, items []ListItem) (image.Image, error) {
+
+}
