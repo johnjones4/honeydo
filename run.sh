9956dddfcc660fb57dce68365f2b773bc127c048
John Jones <johnjones@battlestation.house.johnjonesfour.com>
2021-11-09 17:23:11 -0500

    added timeout

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ while true
 do
   /usr/local/bin/go/bin/go run .
   # convert -rotate "-90" rendered.bmp rendered.bmp
-  sudo IT8951/IT8951 0 0 rendered.bmp
+  timeout 60 sudo IT8951/IT8951 0 0 rendered.bmp
   rm rendered.bmp
   sleep 300
 done
